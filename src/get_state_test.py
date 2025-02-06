25fd90a1b0c07b064ae426413175ed787c05c81c
Leevi Salin <leevi.salin@aalto.fi>
2025-02-06 12:10:26 +0200

    copied an example from the sdk

---
+++ src/get_state_test.py
@@ -0,0 +1,40 @@
+
+"""Copied from examples/get_robot_state.py """
+
+import sys
+
+import bosdyn.client
+import bosdyn.client.util
+from bosdyn.client.robot_state import RobotStateClient
+
+
+def main():
+    import argparse
+
+    commands = {'state', 'hardware', 'metrics'}
+
+    parser = argparse.ArgumentParser()
+    bosdyn.client.util.add_base_arguments(parser)
+    parser.add_argument('command', choices=list(commands), help='Command to run')
+    options = parser.parse_args()
+
+    # Create robot object with an image client.
+    sdk = bosdyn.client.create_standard_sdk('RobotStateClient')
+    robot = sdk.create_robot(options.hostname)
+    bosdyn.client.util.authenticate(robot)
+    robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
+
+    # Make a robot state request
+    if options.command == 'state':
+        print(robot_state_client.get_robot_state())
+    elif options.command == 'hardware':
+        print(robot_state_client.get_hardware_config_with_link_info())
+    elif options.command == 'metrics':
+        print(robot_state_client.get_robot_metrics())
+
+    return True
+
+
+if __name__ == '__main__':
+    if not main():
+        sys.exit(1)
