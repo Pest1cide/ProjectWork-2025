e9cece4e05a43a069ae359995002909018221ea8
Pest1side <zimeng.wang@aalto.fi>
2025-03-26 13:59:04 +0200

    first training result with fixed arm

---
+++ examples/locomotion/spot_train.py
@@ -0,0 +1,185 @@
+import argparse
+import os
+import pickle
+import shutil
+
+from spot_env import SpotEnv
+from rsl_rl.runners import OnPolicyRunner
+
+import genesis as gs
+
+
+def get_train_cfg(exp_name, max_iterations):
+
+    train_cfg_dict = {
+        "algorithm": {
+            "clip_param": 0.2,
+            "desired_kl": 0.01,
+            "entropy_coef": 0.01,
+            "gamma": 0.99,
+            "lam": 0.95,
+            "learning_rate": 0.001,
+            "max_grad_norm": 1.0,
+            "num_learning_epochs": 5,
+            "num_mini_batches": 4,
+            "schedule": "adaptive",
+            "use_clipped_value_loss": True,
+            "value_loss_coef": 1.0,
+        },
+        "init_member_classes": {},
+        "policy": {
+            "activation": "elu",
+            "actor_hidden_dims": [512, 256, 128],
+            "critic_hidden_dims": [512, 256, 128],
+            "init_noise_std": 1.0,
+        },
+        "runner": {
+            "algorithm_class_name": "PPO",
+            "checkpoint": -1,
+            "experiment_name": exp_name,
+            "load_run": -1,
+            "log_interval": 1,
+            "max_iterations": max_iterations,
+            "num_steps_per_env": 24,
+            "policy_class_name": "ActorCritic",
+            "record_interval": -1,
+            "resume": False,
+            "resume_path": None,
+            "run_name": "",
+            "runner_class_name": "runner_class_name",
+            "save_interval": 100,
+        },
+        "runner_class_name": "OnPolicyRunner",
+        "seed": 1,
+    }
+
+    return train_cfg_dict
+
+
+def get_cfgs():
+    env_cfg = {
+        "num_actions": 12,
+        # joint/link names
+        "default_joint_angles": {  # [rad]
+            "fl_hx": 0.0,
+            "fr_hx": 0.0,
+            "hl_hx": 0.0,
+            "hr_hx": 0.0,
+            "fl_hy": 0.8,
+            "fr_hy": 0.8,
+            "hl_hy": 1.0,
+            "hr_hy": 1.0,
+            "fl_kn": -1.5,
+            "fr_kn": -1.5,
+            "hl_kn": -1.5,
+            "hr_kn": -1.5,
+            "arm0_sh0": 0.05785536766052246,
+            "arm0_sh1": -3.118537187576294, 
+            "arm0_hr0": 0.0, 
+            "arm0_el0": 3.136108875274658, 
+            "arm0_el1": 1.5643538236618042, 
+            "arm0_wr0": 0.33551907539367676, 
+            "arm0_wr1": -1.7799558639526367, 
+            "arm0_f1x": -0.278841495513916
+
+        },
+        "dof_names": [
+            "fr_hx",
+            "fr_hy",
+            "fr_kn",
+            "fl_hx",
+            "fl_hy",
+            "fl_kn",
+            "hr_hx",
+            "hr_hy",
+            "hr_kn",
+            "hl_hx",
+            "hl_hy",
+            "hl_kn",
+
+        ],
+        # PD
+        "kp": 20.0,
+        "kd": 0.5,
+        # termination
+        "termination_if_roll_greater_than": 45,  # degree
+        "termination_if_pitch_greater_than": 45,
+        # base pose
+        "base_init_pos": [0.0, 0.0, 0.5],
+        "base_init_quat": [1.0, 0.0, 0.0, 0.0],
+        "episode_length_s": 20.0,
+        "resampling_time_s": 4.0,
+        "action_scale": 0.25,
+        "simulate_action_latency": False,
+        "clip_actions": 100.0,
+    }
+    obs_cfg = {
+        "num_obs": 45,
+        "obs_scales": {
+            "lin_vel": 2.0,
+            "ang_vel": 0.25,
+            "dof_pos": 1.0,
+            "dof_vel": 0.05,
+        },
+    }
+    reward_cfg = {
+        "tracking_sigma": 0.25,
+        "base_height_target": 0.45,
+        "feet_height_target": 0.1,
+        "reward_scales": {
+            "tracking_lin_vel": 1.0,
+            "tracking_ang_vel": 0.2,
+            "lin_vel_z": -1.0,
+            "base_height": -50.0,
+            "action_rate": -0.005,
+            "similar_to_default": -0.1,
+        },
+    }
+    command_cfg = {
+        "num_commands": 3,
+        "lin_vel_x_range": [-0.5, 0.5],
+        "lin_vel_y_range": [-0.5, 0.5],
+        "ang_vel_range": [0, 0],
+    }
+
+    return env_cfg, obs_cfg, reward_cfg, command_cfg
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--exp_name", type=str, default="spot-walking")
+    parser.add_argument("-B", "--num_envs", type=int, default=4096)
+    parser.add_argument("--max_iterations", type=int, default=1000)
+    args = parser.parse_args()
+
+    gs.init(logging_level="warning")
+
+    log_dir = f"logs/{args.exp_name}"
+    env_cfg, obs_cfg, reward_cfg, command_cfg = get_cfgs()
+    train_cfg = get_train_cfg(args.exp_name, args.max_iterations)
+
+    if os.path.exists(log_dir):
+        shutil.rmtree(log_dir)
+    os.makedirs(log_dir, exist_ok=True)
+
+    env = SpotEnv(
+        num_envs=args.num_envs, env_cfg=env_cfg, obs_cfg=obs_cfg, reward_cfg=reward_cfg, command_cfg=command_cfg, show_viewer=True
+    )
+
+    runner = OnPolicyRunner(env, train_cfg, log_dir, device="cuda:0")
+
+    pickle.dump(
+        [env_cfg, obs_cfg, reward_cfg, command_cfg, train_cfg],
+        open(f"{log_dir}/cfgs.pkl", "wb"),
+    )
+
+    runner.learn(num_learning_iterations=args.max_iterations, init_at_random_ep_len=True)
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+# training
+python examples/locomotion/go2_train.py
+"""
